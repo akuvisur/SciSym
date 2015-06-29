823be74c27970e0ab0628870a82a6af23c72f6ff
Aku <aku.visuri@invian.fi>
2015-06-29 18:13:32 +0300

    Symptom creator UI

---
+++ app/src/main/java/com/akuvisuri/scisym/view/FactorCreator.java
@@ -0,0 +1,7 @@
+package com.akuvisuri.scisym.view;
+
+/**
+ * Created by Aku on 29.6.2015.
+ */
+public class FactorCreator {
+}
---
+++ app/src/main/java/com/akuvisuri/scisym/view/SymptomCreator.java
@@ -0,0 +1,20 @@
+package com.akuvisuri.scisym.view;
+
+import android.app.Activity;
+
+import com.akuvisuri.scisym.trackables.Symptom;
+
+/**
+ * Created by Aku on 29.6.2015.
+ */
+public class SymptomCreator {
+
+    private static Symptom s;
+
+    public static Symptom show(Activity a) {
+        s = null;
+
+        return new Symptom()
+    }
+
+}
